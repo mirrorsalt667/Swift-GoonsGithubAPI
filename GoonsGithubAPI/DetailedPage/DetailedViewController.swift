a98460bd224c7c585006f70415e1c276405407db
StephenMBAM1 <jjjjllears@icloud.com>
2023-10-14 21:05:30 +0800

    docs: 建立專案。
    版本支援iOS13.0以上，並以資料夾區分頁面與功能。

---
+++ GoonsGithubAPI/DetailedPage/DetailedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailedViewController.swift
+//  GoonsGithubAPI
+//
+//  Created by Stephen on 2023/10/14.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GoonsGithubAPI/SourceFetcher/SourceFetcher.swift
@@ -0,0 +1,8 @@
+//
+//  SourceFetcher.swift
+//  GoonsGithubAPI
+//
+//  Created by Stephen on 2023/10/14.
+//
+
+import Foundation
